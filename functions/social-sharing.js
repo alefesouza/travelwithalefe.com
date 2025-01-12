a8131cafde88168a88f439e00ff94511560025f7
Alefe Souza <contact@alefesouza.com>
2025-01-12 16:06:53 -0300

    Add multi language support on social sharing

--- functions/social-sharing.js
+++ functions/social-sharing.js
@@ -9,7 +9,7 @@ initializeApp({
   credential: cert(serviceAccount),
 });
 
-async function createPost(text) {
+async function createPost() {
   const firestore = getFirestore();
   const storage = getStorage();
   const bucket = storage.bucket('files.viajarcomale.com');
@@ -23,49 +23,131 @@ async function createPost(text) {
   const items = [];
 
   compilationsSnaptshot.forEach((doc) => {
-    items.push(doc.data());
-  });
+    const data = doc.data();
 
-  for (const item of items.slice(0, 1)) {
-    const promises = [];
-
-    const files = [
-      item.file,
-      ...item.gallery
-        .filter((item) => !item.file_type.includes('.mp4'))
-        .map((item) => item.file),
-    ].slice(0, 4);
-
-    for (const file of files) {
-      promises.push(
-        new Promise((resolve, reject) => {
-          const chunks = [];
-
-          bucket
-            .file(file.substring(1))
-            .createReadStream() //stream is created
-            .on('data', (data) => {
-              chunks.push(data);
-            })
-            .on('finish', async () => {
-              resolve(chunks);
-            });
-        })
-      );
+    if (data.bluesky_id_pt && data.mastodon_id_pt && data.twitter_id_pt) {
+      return;
     }
 
-    const allChunks = await Promise.all(promises);
+    items.push({
+      ...data,
+      path: doc.ref.path,
+    });
+  });
 
-    const description = `${item.description}\n.\n.\n.\n#${item.hashtags.join(
+  const item = items[0];
+
+  const promises = [];
+
+  const files = [
+    item.file,
+    ...item.gallery
+      .filter((item) => !item.file_type.includes('.mp4'))
+      .map((item) => item.file),
+  ].slice(0, 4);
+
+  for (const file of files) {
+    promises.push(
+      new Promise((resolve) => {
+        const chunks = [];
+
+        bucket
+          .file(file.substring(1))
+          .createReadStream() //stream is created
+          .on('data', (data) => {
+            chunks.push(data);
+          })
+          .on('finish', async () => {
+            resolve(chunks);
+          });
+      })
+    );
+  }
+
+  const allChunks = await Promise.all(promises);
+
+  const siteLink = `https://travelwithalefe.com/countries/${
+    item.country
+  }/cities/${item.city}/posts/${item.id.replace(`${item.city}-post-`, '')}`;
+  const siteLinkPt = `https://viajarcomale.com.br/countries/${
+    item.country
+  }/cities/${item.city}/posts/${item.id.replace(`${item.city}-post-`, '')}`;
+
+  let description = `${item.description.substring(0, 300)}… ${siteLink}`;
+
+  if (item.description.length < 250) {
+    description = `${item.description}\n.\n.\n.\n#${item.hashtags.join(
       ' #'
-    )} https://travelwithalefe.com/countries/${item.country}/cities/${
-      item.city
-    }/posts/${item.id.replace(`${item.city}-post-`, '')}`;
+    )} ${siteLink}`;
 
-    bluesky.post(item, description, allChunks);
-    mastodon.post(item, description, allChunks);
-    twitter.post(item, description, allChunks);
+    while (description.length >= 300 && item.hashtags.length > 0) {
+      description = description.replace(
+        ' #' + item.hashtags[item.hashtags.length - 1],
+        ''
+      );
+
+      item.hashtags.pop();
+    }
+  }
+
+  let descriptionPt = `${item.description_pt.substring(0, 300)}… ${siteLinkPt}`;
+
+  if (item.description_pt.length < 250) {
+    descriptionPt = `${item.description_pt}\n.\n.\n.\n#${item.hashtags_pt.join(
+      ' #'
+    )} ${siteLinkPt}`;
+
+    while (descriptionPt.length >= 300 && item.hashtags.length > 0) {
+      descriptionPt = descriptionPt.replace(
+        ' #' + item.hashtags_pt[item.hashtags_pt.length - 1],
+        ''
+      );
+
+      item.hashtags_pt.pop();
+    }
+  }
+
+  if (!item.bluesky_id || !item.bluesky_id_pt) {
+    const blueskyId = await bluesky.post(
+      !!item.bluesky_id,
+      item,
+      description,
+      descriptionPt,
+      allChunks
+    );
+
+    firestore.doc(item.path).update({
+      [item.bluesky_id ? 'bluesky_id_pt' : 'bluesky_id']: blueskyId,
+    });
+  }
+
+  if (!item.mastodon_id || !item.mastodon_id_pt) {
+    const mastodonId = await mastodon.post(
+      !!item.mastodon_id,
+      item,
+      description,
+      descriptionPt,
+      allChunks
+    );
+
+    firestore.doc(item.path).update({
+      [item.mastodon_id ? 'mastodon_id_pt' : 'mastodon_id']: mastodonId,
+    });
+  }
+
+  if (!item.twitter_id || !item.twitter_id_pt) {
+    const twitterId = await twitter.post(
+      !!item.twitter_id,
+      item,
+      description,
+      descriptionPt,
+      allChunks
+    );
+
+    firestore.doc(item.path).update({
+      [item.twitter_id ? 'twitter_id_pt' : 'twitter_id']: twitterId,
+    });
   }
 }
 
-createPost('Hello World');
+createPost();
--- functions/social.js
+++ functions/social.js
@@ -3,23 +3,54 @@ const { createRestAPIClient } = require('masto');
 const { TwitterApi, EUploadMimeType } = require('twitter-api-v2');
 const path = require('path');
 
+const travelWithAlefe = {
+  bluesky: {
+    identifier: 'travelwithalefe@as.dev',
+    password: 'g@3Y.!Kb4JqpspMzW4Nb',
+  },
+  mastodon: {
+    url: 'https://mastodon.social',
+    accessToken: 'PQIRFliXg8s8jL9WHEj2tzyzMFMDYWq8P2ttSWnmTEc',
+  },
+  twitter: {
+    appKey: 'OpyXLafpumLSmzHgo0gOLsvCF',
+    appSecret: 'FLIw1o3xFvIHcwAGhNC3V3Hsd0PO2BJgfitWxqaaoK1N0ElgZw',
+    accessToken: '1693265790718357504-BIFUqVXJUvjESCApwfTLpSSrNyV6Cf',
+    accessSecret: 'HyyWNP0KjwML6ZXs7sk4dFg5J7Cq78hxtU6gWa98zxaQZ',
+  },
+};
+
+const viajarComAle = {
+  bluesky: {
+    identifier: 'contact@alefesouza.com',
+    password: 'frg2djv!kdg3fge2HYM',
+  },
+  mastodon: {
+    url: 'https://mastodon.social',
+    accessToken: 'aPsROG1XDXiEucIGDDunk_LeExI66s16p8RUB0ZmZPU',
+  },
+  twitter: {
+    appKey: 'u94IArl6frkXUkkiVK0lobLA5',
+    appSecret: 'D6fm1OWvIZFffAlWuL32wFFoEVRDKVy8OT9phtHXMpTd6ICjPm',
+    accessToken: '1693645649789480960-jvEmqw5610mx6Z4AWgoTyW8envGb4G',
+    accessSecret: 'fWxXA4SSzFzTbsVT1bAuqlh5zrAktiBlpvGI1GgCS5OqM',
+  },
+};
+
 const bluesky = {
-  async authenticate() {
+  async authenticate(isBR) {
     const agent = new AtpAgent({ service: 'https://bsky.social' });
 
-    await agent.login({
-      identifier: 'travelwithalefe@as.dev',
-      password: 'g@3Y.!Kb4JqpspMzW4Nb',
-    });
+    await agent.login(isBR ? viajarComAle.bluesky : travelWithAlefe.bluesky);
 
     return agent;
   },
 
-  async post(item, description, allChunks) {
+  async post(isBR, item, description, descriptionPt, allChunks) {
     const images = await Promise.all(
       allChunks.map(async (chunks) => {
         return new Promise(async (resolve, reject) => {
-          const agent = await this.authenticate();
+          const agent = await this.authenticate(isBR);
 
           const {
             data: { blob: image },
@@ -30,10 +61,10 @@ const bluesky = {
       })
     );
 
-    const agent = await this.authenticate();
+    const agent = await this.authenticate(isBR);
 
     const rt = new RichText({
-      text: description,
+      text: isBR ? descriptionPt : description,
     });
     await rt.detectFacets(agent); // automatically detects mentions and links
 
@@ -48,7 +79,7 @@ const bluesky = {
           $type: 'app.bsky.embed.images',
           images: images.map((image) => ({
             image,
-            alt: item.description,
+            alt: isBR ? item.description_pt : item.description,
             aspectRatio: {
               // a hint to clients
               width: item.width,
@@ -59,6 +90,8 @@ const bluesky = {
       }
     );
 
+    console.log('bluesky', res3);
+
     const blueskyId = path.basename(res3.uri);
 
     return blueskyId;
@@ -66,24 +99,23 @@ const bluesky = {
 };
 
 const mastodon = {
-  authenticate() {
-    const masto = createRestAPIClient({
-      url: 'https://mastodon.social',
-      accessToken: 'PQIRFliXg8s8jL9WHEj2tzyzMFMDYWq8P2ttSWnmTEc',
-    });
+  authenticate(isBR) {
+    const masto = createRestAPIClient(
+      isBR ? viajarComAle.mastodon : travelWithAlefe.mastodon
+    );
 
     return masto;
   },
 
-  async post(item, description, allChunks) {
-    const masto = this.authenticate();
+  async post(isBR, item, description, descriptionPt, allChunks) {
+    const masto = this.authenticate(isBR);
 
     const attachments = await Promise.all(
       allChunks.map(async (chunks) => {
         return new Promise(async (resolve, reject) => {
           const attachment = await masto.v2.media.create({
             file: new Blob(chunks),
-            description: item.description,
+            description: isBR ? item.description_pt : item.description,
           });
 
           resolve(attachment.id);
@@ -92,29 +124,28 @@ const mastodon = {
     );
 
     const status = await masto.v1.statuses.create({
-      status: description,
+      status: isBR ? descriptionPt : description,
       visibility: 'public',
       mediaIds: attachments,
     });
+
+    console.log('mastodon', status);
 
     return status.id;
   },
 };
 
 const twitter = {
-  authenticate() {
-    const client = new TwitterApi({
-      appKey: 'OpyXLafpumLSmzHgo0gOLsvCF',
-      appSecret: 'FLIw1o3xFvIHcwAGhNC3V3Hsd0PO2BJgfitWxqaaoK1N0ElgZw',
-      accessToken: '1693265790718357504-BIFUqVXJUvjESCApwfTLpSSrNyV6Cf',
-      accessSecret: 'HyyWNP0KjwML6ZXs7sk4dFg5J7Cq78hxtU6gWa98zxaQZ',
-    });
+  authenticate(isBR) {
+    const client = new TwitterApi(
+      isBR ? viajarComAle.twitter : travelWithAlefe.twitter
+    );
 
     return client;
   },
 
-  async post(item, description, allChunks) {
-    const client = this.authenticate();
+  async post(isBR, item, description, descriptionPt, allChunks) {
+    const client = this.authenticate(isBR);
 
     const twitterClient = client.readWrite;
 
@@ -131,11 +162,13 @@ const twitter = {
     );
 
     const result = await twitterClient.v2.tweet({
-      text: description,
+      text: isBR ? descriptionPt : description,
       media: { media_ids: mediaIds },
     });
 
-    return result.id;
+    console.log('twitter', result);
+
+    return result.data.id;
   },
 };
 
